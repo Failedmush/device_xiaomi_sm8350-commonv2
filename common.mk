9b61ae7b012295831b6a0eaed40365e2460a370e
Localhorst04 <aaron.laib@protonmail.com>
2025-06-27 13:02:43 +0000

    sm8350-common: Add libaudiosystem_shim to resolve missing symbol
    Signed-off-by: Hexdare <mohammedasimuddin786@gmail.com>
    
    Introduce the libaudiosystem_shim to address the unresolved symbol:
    _ZN7android11AudioSystem24setDeviceConnectionStateE24audio_policy_dev_state_tRKNS_5media5audio6common9AudioPortE14audio_format_t
    
    The symbol corresponds to the method signature:
    android::AudioSystem::setDeviceConnectionState(audio_policy_dev_state_t, android::media::audio::common::AudioPort const&, audio_format_t)
    
    This maps to AudioSystem::setDeviceConnectionState in frameworks/av/media/libaudioclient/AudioSystem.cpp:
    status_t AudioSystem::setjpDeviceConnectionState(
    audio_policy_dev_state_t state,
    const android::media::audio::common::AudioPort& port,
    audio_format_t encodedFormat,
    bool deviceSwitch)
    
    The discrepancy arises because the commit
    VoltageOS-staging/frameworks_av@7099778
    introduced a bool deviceSwitch parameter to the AudioSystem::setDeviceConnectionState method, which libwfdservice.so does not account for.
    
    About deviceSwitch (bool):
    - true: Immediately switch the audio output to the specified device.
    - false: Register the connection without automatically switching audio output.
    
    The decision has been made to leave deviceSwitch set to false as a conservative approach, given the behavior it controls

--- common.mk
+++ common.mk
@@ -373,6 +373,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
+# Shims
+PRODUCT_PACKAGES += \
+    libcrypto-v34 \
+    libaudiosystem_shim
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
